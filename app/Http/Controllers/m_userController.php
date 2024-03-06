d9c6a1047f37bdf4f1bbe084c067747b0415e568
NaufalSyahandra <114036607+NaufalSyahandra@users.noreply.github.com>
2024-03-06 23:31:18 +0700

    Jobsheet 4/Praktikum 2.1/Retrieving Single Models

--- app/Http/Controllers/m_userController.php
+++ app/Http/Controllers/m_userController.php
@@ -27,16 +27,33 @@ class m_userController extends Controller
 //        ];
 //        m_userModel::create($data);
 
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        m_userModel::create($data);
+//        $data = [
+//            'level_id' => 2,
+//            'username' => 'manager_tiga',
+//            'nama' => 'Manager 3',
+//            'password' => Hash::make('12345')
+//        ];
+//        m_userModel::create($data);
+//
+//
+//        $user = m_userModel::all(); // get all data from table m_user
+//        return view('m_user', ['data' => $user]);
 
+//        $user = m_userModel::find(1);
+//        return view('m_user', ['data' => $user]);
 
-        $user = m_userModel::all(); // get all data from table m_user
+//        $user = m_userModel::where('level_id', 1)->first();
+//        return view('m_user', ['data' => $user]);
+
+//        $user = m_userModel::findOr(1, ['username', 'nama'], function () {
+//            abort(404);
+//        });
+//        return view('m_user', ['data' => $user]);
+
+        $user = m_userModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
         return view('m_user', ['data' => $user]);
+
     }
 }
