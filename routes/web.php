a5834205dd934b4edff387517102702cc1ac4cc5
NaufalSyahandra <114036607+NaufalSyahandra@users.noreply.github.com>
2024-04-23 08:51:39 +0700

    JS 9/Auth

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ManagerController;
 use App\Http\Controllers\m_kategoriController;
 use App\Http\Controllers\m_userController;
 use Illuminate\Support\Facades\Route;
@@ -53,3 +56,32 @@ Route::post('stok/list', [t_stokController::class, 'list']);
 
 Route::resource('penjualan', TransaksiPenjualanController::class);
 Route::post('penjualan/list', [TransaksiPenjualanController::class, 'list']);
+
+/**
+ * Routes for User Authentication process
+ */
+Route::get('login', [AuthController::class, 'index'])->name('login');
+Route::get('register', [AuthController::class, 'register'])->name('register');
+Route::post('proses_login', [AuthController::class, 'proses_login'])->name('proses_login');
+Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+Route::post('proses_register', [AuthController::class, 'proses_register'])->name('proses_register');
+
+/**
+ * use Authentication class using middleware aliases in http/kernel
+ * to redirect users when they are not authenticate
+ */
+Route::group(['middleware' => ['auth']], function () {
+
+    /**
+     * if user is admin
+     */
+    Route::group(['middleware' => ['cek_login:1']], function () {
+        Route::resource('admin', AdminController::class);
+    });
+    /**
+     * if user is manager
+     */
+    Route::group(['middleware' => ['cek_login:2']], function () {
+        Route::resource('manager', ManagerController::class);
+    });
+});
